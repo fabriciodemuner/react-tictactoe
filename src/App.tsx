6f66bc2bbff7759d0b744db07ed4a692952018a6
fabricio.demuner <fabricio.demuner@gmail.com>
2021-02-11 12:00:26 +0100

    change api_host variable name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { HOST } from "./config/default";
 import { Game, Result, Tiles } from "./Game";
 import { Player } from "./TicTile";
 
-console.log(process.env.NEXT_PUBLIC_API_HOST, HOST);
+console.log(process.env.REACT_APP_API_HOST, HOST);
 const socket = io(HOST);
 export type Score = {
   O: number;
--- src/config/default.js
+++ src/config/default.js
@@ -1,1 +1,1 @@
-export const HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:5000";
+export const HOST = process.env.REACT_APP_API_HOST || "http://localhost:5000";
